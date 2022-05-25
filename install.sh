bdfc907ba84d7dfb6f6339f370e5f12d2d8eacc1
maradeim <105745546+maradeim@users.noreply.github.com>
2022-05-25 22:51:25 +0300

    add: win32yank install script

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
+# Stuff
+sudo apt -y install unzip
 sudo apt -y install build-essential
+sudo apt -y install manpages-dev
+
+# ripgrep
+sudo apt-get install ripgrep
+sudo apt-get install fzf
+sudo apt-get install fd-find
 
 # Node latest lts
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
@@ -9,6 +17,15 @@ nvm install --lts
 
 # Global TS
 npm install -g typescript typescript-language-server
+
+
+
+# win32yank.exe for vim/nvim
+curl -sLo /tmp/win32yank.zip https://github.com/equalsraf/win32yank/releases/download/v0.0.4/win32yank-x64.zip
+unzip -p /tmp/win32yank.zip win32yank.exe > /tmp/win32yank.exe
+chmod +x /tmp/win32yank.exe
+sudo mv /tmp/win32yank.exe /usr/local/bin/
+
 
 # TODO: 
 # Go
